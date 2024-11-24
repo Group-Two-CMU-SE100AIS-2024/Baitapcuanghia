d3608f4e0cd0e456065532035fafe02af3c30f70
toxuannghia <huyanbeo123@gmail.com>
2024-11-24 12:10:30 +0700

    Add project files.

---
+++ Baitapcuanghia.Module/BusinessObjects/ORMDataModel1Code/Phieuchi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+namespace Baitapcuanghia.Module.BusinessObjects.ORMDataModel1
+{
+
+    public partial class Phieuchi
+    {
+        public Phieuchi(Session session) : base(session) { }
+        public override void AfterConstruction()
+        { 
+            base.AfterConstruction();
+            if (Session.IsNewObject(this))
+            {
+                Ngay= DateTime.Now;
+            } 
+                
+        }
+    }
+
+}
